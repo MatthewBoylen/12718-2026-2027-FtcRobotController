020741b7805e695ad686e71a5cb4c092edca09ca
g28wenic <g28wenic@saes.org>
2026-09-14 12:55:45 -0400

    Updated name and group to be more descriptive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainDrive.java
@@ -1,19 +1,10 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
-import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.PwmControl;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.ServoImplEx;
-import com.qualcomm.robotcore.hardware.VoltageSensor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-@TeleOp(name="Drive", group="Drive")
-public class Drive extends LinearOpMode {
+@TeleOp(name="mainDrive", group="mainDrive")
+public class mainDrive extends LinearOpMode {
     private DcMotor frontLeft, backLeft, frontRight, backRight;
     @Override
     public void runOpMode() {
